1144ad8cad3019e011bad75c817e7684144df866
kavyaprakas <119047808+kavyaprakas@users.noreply.github.com>
2022-11-25 16:37:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DynamicForm from './Components/form';
+
+function App() {
+  return (
+    <div className="App">
+      <DynamicForm></DynamicForm>
+    </div>
+  );
+}
+
+export default App;
